a2fa5db687c451fde3c0607152965a98a8d99c75
jeromedelizo188 <jeromedelizo9@gmail.com>
2026-09-15 22:19:59 +0800

    building apk 6

--- src/components/CameraComponent.vue
+++ src/components/CameraComponent.vue
@@ -5,9 +5,6 @@
             <ion-button expand="block" @click="takePicture"> 
                 <ion-icon slot ="start" :icon="cameraIcon" /> Take Picture
             </ion-button>
-            <ion-text v-if="statusMessage">
-                <p>{{ statusMessage }}</p>
-            </ion-text>
             <ion-text v-if="errorMessage" color="danger">
                 <p>{{ errorMessage }}</p>
             </ion-text>
@@ -28,26 +25,21 @@ import { camera as cameraIcon } from "ionicons/icons";
 import { Camera } from "@capacitor/camera";
 import { ref }  from "vue";
 const errorMessage = ref("");
-const statusMessage = ref("");
 const emit = defineEmits<{ (event: "photoCaptured", photo: string): void }> ();
 const takePicture = async () => {
     errorMessage.value = "";
-    statusMessage.value = "";
     try {
         const photo = await Camera.takePhoto ({ quality: 90, saveToGallery: false });
-        statusMessage.value = `Keys: ${Object.keys(photo).join(', ')} | thumb: ${photo.thumbnail ? 'yes (' + photo.thumbnail.length + ')' : 'no'} | webPath: ${photo.webPath ? 'yes' : 'no'}`;
         const mime = photo.thumbnail?.startsWith("/9j/") ? "image/jpeg" : "image/png";
         const imageSrc = photo.thumbnail
             ? `data:${mime};base64,${photo.thumbnail}`
             : photo.webPath;
         if (imageSrc) {
             emit("photoCaptured", imageSrc);
-        } else {
-            statusMessage.value += " | NO SOURCE TO DISPLAY";
         }
     } catch (error) {
       console.error(error);
-      errorMessage.value = `Error: ${error instanceof Error ? error.message : String(error)}`;
+      errorMessage.value = "Unable to capture photo";
     }
 }
 </script>
